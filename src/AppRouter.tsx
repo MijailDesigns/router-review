91e684ad42533026a16a4a73ec56da4d5126b6b4
Mijail Pulgar <mijail86@gmail.com>
2025-05-16 23:37:37 -0500

    feat: lazy loading

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,6 +3,15 @@ import AuthLayout from "./auth/layout/AuthLayout";
 import { LoginPage } from "./auth/pages/LoginPage";
 import { RegisterPage } from "./auth/pages/RegisterPage";
 
+import ChatPage from "./chat/pages/ChatPage";
+import { lazy, Suspense } from "react";
+import { sleep } from "./lib/sleep";
+
+const ChatLayout = lazy(async () => {
+  await sleep(1500);
+  return import("./chat/layout/ChatLayout");
+});
+
 export const AppRouter = () => {
   return (
     <BrowserRouter>
@@ -10,6 +19,23 @@ export const AppRouter = () => {
         <Route path="/auth" element={<AuthLayout />}>
           <Route index element={<LoginPage />} />
           <Route path="/auth/register" element={<RegisterPage />} />
+        </Route>
+
+        <Route
+          path="/chat"
+          element={
+            <Suspense
+              fallback={
+                <div className="flex justify-center items-center h-screen">
+                  <div className="spinner-border animate-spin rounded-full border-4 border-gray-200 border-t-4 border-t-purple-600 h-12 w-12"></div>
+                </div>
+              }
+            >
+              <ChatLayout />
+            </Suspense>
+          }
+        >
+          <Route index element={<ChatPage />} />
         </Route>
 
         <Route path="/" element={<Navigate to="/auth" />} />
