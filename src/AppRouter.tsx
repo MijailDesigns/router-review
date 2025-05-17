c5eafff7c933d8ae5e245dce916588decef60756
Mijail Pulgar <mijail86@gmail.com>
2025-05-16 22:42:48 -0500

    feat: react router instalacion y configuracion

---
+++ src/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import AuthLayout from "./auth/layout/AuthLayout";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          {/* <Route path="login" element{<Login/>}/>
+                    <Route path="register" element{<Register/>}/> */}
+        </Route>
+
+        <Route path="/" element={<Navigate to="/auth" />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
